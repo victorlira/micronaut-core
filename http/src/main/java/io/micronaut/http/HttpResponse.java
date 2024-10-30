8096e16a95f6e2489dc483a43f7353f846ee6054
Sergio del Amo <sergio.delamo@softamo.com>
2024-10-30 12:17:03 +0100

    Update http/src/main/java/io/micronaut/http/HttpResponse.java

--- http/src/main/java/io/micronaut/http/HttpResponse.java
+++ http/src/main/java/io/micronaut/http/HttpResponse.java
@@ -36,7 +36,7 @@ public interface HttpResponse<B> extends HttpMessage<B> {
 
     /**
      * @return The current status
-     * @deprecated To support custom status codes. Use {@link #code()} instead of {@link #getStatus()} and {@link HttpStatus#getCode()}  and {@link #reason()} instead of {@link #getStatus()} and {@link HttpStatus#getReason()}
+     * To support custom status codes. Use {@link #code()} instead of {@link #getStatus()} and {@link HttpStatus#getCode()}  and {@link #reason()} instead of {@link #getStatus()} and {@link HttpStatus#getReason()}
      */
     default HttpStatus getStatus() {
         return HttpStatus.valueOf(code());
