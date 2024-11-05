2c83acd3f814f0f230621b66ae5a7f2b55c5694e
radovanradic <radicr@gmail.com>
2024-11-05 16:04:43 +0100

    Inner classes with builder don't get introspected

--- test-suite/src/test/java/io/micronaut/test/lombok/LombokIntrospectedBuilderTest.java
+++ test-suite/src/test/java/io/micronaut/test/lombok/LombokIntrospectedBuilderTest.java
@@ -4,6 +4,8 @@ import io.micronaut.core.beans.BeanIntrospection;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 public class LombokIntrospectedBuilderTest {
 
     @Test
@@ -15,5 +17,21 @@ public class LombokIntrospectedBuilderTest {
             .build();
 
         Assertions.assertEquals("foo", robotEntity.getName());
+    }
+
+    @Test
+    void testLombokBuilderWithInnerClasses() {
+        BeanIntrospection.Builder<SimpleEntity> builder = BeanIntrospection.getIntrospection(SimpleEntity.class)
+            .builder();
+
+        String id = UUID.randomUUID().toString();
+        SimpleEntity simpleEntity = builder.with("id", id)
+            .build();
+
+        Assertions.assertEquals(id, simpleEntity.getId());
+
+        BeanIntrospection<SimpleEntity.CompartmentCreationTimeIndexPrefix> introspection =
+            BeanIntrospection.getIntrospection(SimpleEntity.CompartmentCreationTimeIndexPrefix.class);
+        Assertions.assertNotNull(introspection);
     }
 }
